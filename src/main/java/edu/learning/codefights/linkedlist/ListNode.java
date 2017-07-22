2d731840d885c0f00133771b529aa80d52c8102e
Nguyen Huu Duc <duchuunguyen@Nguyens-MacBook-Pro.local>
2017-07-21 22:14:36 -0400

    Linked List - Add 2 Huge Numbers

---
+++ src/main/java/edu/learning/codefights/linkedlist/ListNode.java
@@ -0,0 +1,13 @@
+package edu.learning.codefights.linkedlist;
+
+/**
+ * Created by duchuunguyen on 7/21/17.
+ */
+class ListNode<T> {
+    ListNode(T x) {
+        value = x;
+    }
+
+    T value;
+    ListNode<T> next;
+}
